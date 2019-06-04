1d514e33869e9dcf05de2278c30a636b2f1e1b5c
Rohan Vembar <rohanvembar@gmail.com>
2019-06-03 18:14:31 -0700

    add some sql statements

---
+++ SQL Statements/statements_rohan_not_final.sql
@@ -0,0 +1,28 @@
+--- The system shall display the list of reservations made by a user including active and past reservations.
+select firstName, lastName, roomName, checkIn, checkOut, rate from Reservations join Customers on Reservations.custID = Customers.id join Rooms on Reservations.roomID = Rooms.roomID where firstName = ? and lastname = ?
+---?1: first name
+---?2: last name
+
+
+--- Upon the cancellation or change of a reservation, the system shall display the details of the cancelled or changed reservation on the screen.
+select Reservations.id, firstName, lastName, roomName, checkIn, checkOut, rate from Reservations join Customers on Reservations.custID = Customers.id join Rooms on Reservations.roomID = Rooms.roomID where Reservations.id = ?
+---?1: reservation id passed in from change function
+
+
+--- Upon reservation, the system shall display the details of the reservation on the Screen.
+select Reservations.id, firstName, lastName, roomName, checkIn, checkOut, rate from Reservations join Customers on Reservations.custID = Customers.id join Rooms on Reservations.roomID = Rooms.roomID where Reservations.id = ?
+---?1: reservation id passed in from create function
+
+
+--- The system shall allow a user to change reservations such as changing datesand changing rooms.
+update Reservations set roomID = ?, checkIn = ?, checkout = ? where id = ?
+---?1: roomID (if needed)
+---?2: checkin (if needed)
+---?3: check out (if needed)
+---?4: reservation id
+
+
+--- The system shall allow users to search for availabilities of rooms specifying day
+---(checkout and checkin dates), the type of room (single, double, twin, etc), the
+---decor, the price range, the number of rooms, and the number of occupants.
+select roomName, numBeds, bedType, maxOccupancy, rate, decor from Rooms where bedType = ? and decor = ? and rate < ? and rate > ? and maxOccupancy >= ?
